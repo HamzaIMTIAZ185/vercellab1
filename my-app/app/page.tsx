5652cc0bb7badf4f474b55d27193cf60dbafe3a2
Hamza Imtiaz <hamzaimtiazaaa@gmail.com>
2026-05-10 01:12:11 +0500

    Initial commit

---
+++ my-app/app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main style={{ 
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#0f173a",
+      color: "greenyellow",
+      fontSize: "40px",
+      fontWeight: "bold"
+    }}>
+      Hamza Imtiaz
+    </main>
+  )
+}
