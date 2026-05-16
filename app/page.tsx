9590547da6283cf7cf8be86a9296dcba9e3f014f
Hamza Imtiaz <hamzaimtiazaaa@gmail.com>
2026-05-17 00:32:46 +0500

    updated the page.tsx command

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,22 @@
-import hero from "@/components/hero";
-import about from "@/components/about";
-import projects from "@/components/projects";
-import academia from "@/components/academia";
-import contact from "@/components/contact";
+import About from "@/components/about";
+import Academia from "@/components/academia";
+import Contact from "@/components/contact";
+import Hero from "@/components/hero";
+import Projects from "@/components/project";
+import Skills from "@/components/skills";
+
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-blue-50 text-black font-sans selection:bg-blue-200">
       {/* We removed navbar and skills entirely */}
-      <hero />
-      <about />
-      <projects />
-      <academia />
-      <contact />
+      <Hero />
+      <About />
+      <Academia />
+      <Contact />
+      <Skills />
+      <Projects />
+      
     </div>
   );
 }
